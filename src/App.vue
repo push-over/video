edadbbdba042fd8860e329b43cff2cf26f709994
高赞 <push_over@163.com>
2019-03-10 20:56:16 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+      <div class="test">
+          dsada
+      </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  
 }
-#nav {
-  padding: 30px;
-}
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="less" scoped>
+ @import './assets/style/common.less';
+ @import './assets/style/mixin.less';
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+
+.test {
+    .sc(20px, red);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import {
+  Button
+} from 'element-ui';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import routes from './router/router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+	routes
+});
+Vue.use(Button);
 
 new Vue({
   router,
